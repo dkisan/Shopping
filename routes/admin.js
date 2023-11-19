e54e0987883697aee22b3048ee570ee4ef3ccc1f
DebasishKisan1 <debasish.kisan@gmail.com>
2023-11-19 08:53:20 +0530

    delete edit update

--- routes/admin.js
+++ routes/admin.js
@@ -8,11 +8,15 @@ const router = express.Router();
 
 // /admin/add-product => GET
 router.get('/add-product', adminController.getAddProduct);
+router.post('/edit-product', adminController.postEditProduct);
+router.get('/edit-product/:productId', adminController.getEditProduct);
 
 // /admin/products => GET
 router.get('/products', adminController.getProducts);
 
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
+
+router.post('/delete-product', adminController.postDeleteProduct);
 
 module.exports = router;
