2a832cbc155054a17db51dcfc298df67ff65e14f
DebasishKisan1 <debasish.kisan@gmail.com>
2023-11-19 12:44:03 +0530

    fetch insert delete

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,8 +14,11 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
   const product = new Product(null, title, imageUrl, description, price);
-  product.save();
-  res.redirect('/');
+  product.save()
+    .then(() => {
+      res.redirect('/');
+    })
+  .catch(err=> console.log(err))
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -24,7 +27,7 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
   const updatedPrice = req.body.price;
   const updatedDescription = req.body.description;
-  const updatedProduct = new Product(prodId, updatedTitle, updatedImageUrl,  updatedDescription, updatedPrice)
+  const updatedProduct = new Product(prodId, updatedTitle, updatedImageUrl, updatedDescription, updatedPrice)
   updatedProduct.save()
   res.redirect('/admin/products')
 };
@@ -32,7 +35,10 @@ exports.postEditProduct = (req, res, next) => {
 exports.postDeleteProduct = (req, res, next) => {
   const prod = req.body.productId
   Product.deleteById(prod)
-  res.redirect('/admin/products')
+  .then(()=>{
+    res.redirect('/admin/products')
+  })
+  .catch(err=>console.log(err))
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -55,11 +61,15 @@ exports.getEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll()
+  .then(([rows, fieldData]) => {
     res.render('admin/products', {
-      prods: products,
+      prods: rows,
       pageTitle: 'Admin Products',
       path: '/admin/products'
     });
-  });
+  })
+  .catch(err => {
+    console.log(err)
+  })
 };
--- models/product.js
+++ models/product.js
@@ -1,21 +1,5 @@
-const fs = require('fs');
-const path = require('path');
+const db = require("../util/database");
 
-const p = path.join(
-  path.dirname(process.mainModule.filename),
-  'data',
-  'products.json'
-);
-
-const getProductsFromFile = cb => {
-  fs.readFile(p, (err, fileContent) => {
-    if (err) {
-      cb([]);
-    } else {
-      cb(JSON.parse(fileContent));
-    }
-  });
-};
 
 module.exports = class Product {
   constructor(id, title, imageUrl, description, price) {
@@ -27,41 +11,19 @@ module.exports = class Product {
   }
 
   save() {
-    getProductsFromFile(products => {
-      if (this.id) {
-        const existingProductIndex = products.findIndex( prod => prod.id === this.id)
-        const updatedProduct = [...products]
-        updatedProduct[existingProductIndex] = this
-        fs.writeFile(p, JSON.stringify(updatedProduct), err => {
-          console.log(err);
-        });
-      } else {
-        this.id = Math.random().toString()
-        products.push(this);
-        fs.writeFile(p, JSON.stringify(products), err => {
-          console.log(err);
-        });
-      }
-    });
+    return db.execute('INSERT INTO products (title,price,description,imageUrl) VALUES (?,?,?,?)', [this.title, this.price, this.description, this.imageUrl])
   }
 
   static fetchAll(cb) {
-    getProductsFromFile(cb);
+    return db.execute('SELECT * FROM products')
   }
 
-  static findById(id, cb) {
-    getProductsFromFile(products => {
-      const product = products.find(p => p.id === id)
-      cb(product)
-    })
+  static findById(id) {
+    return db.execute('SELECT * FROM products where products.id = ?', [id])
   }
 
   static deleteById(id) {
-    getProductsFromFile(products => {
-      const updatedProduct = products.filter(p => p.id !== id)
-      fs.writeFile(p, JSON.stringify(updatedProduct), err => {
-        console.log(err);
-      });
-    })
-  }
-};
+    return db.execute('DELETE FROM products where products.id = ?',[id])
+  };
+
+}
