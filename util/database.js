51365a732349ef7b900ca08d2cdb6eaf14b54ad9
DebasishKisan1 <debasish.kisan@gmail.com>
2023-11-19 17:13:09 +0530

    sequelize setup with model

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2')
+const Sequelize = require('sequelize')
 
-const pool = mysql.createPool({
-    host:'localhost',
-    user:'root',
-    database:'nodecomplete_dk',
-    password:'mysqlroot'
-})
+const sequelize = new Sequelize('nodecomplete_dk','root','mysqlroot',{
+    dialect:'mysql',
+    host:'localhost'
+}) 
 
-module.exports = pool.promise();
+module.exports = sequelize;
